850f1ec3b2be48c1e576119811fdaa2fe8877913
Tan Yen Khoon <tankhoon@gmail.com>
2021-04-09 17:33:01 +0800

    fix linting error.

--- Client/connection.ts
+++ Client/connection.ts
@@ -1,7 +1,7 @@
-import { RpcClient } from "../proto/mangostine_grpc_pb"
-import { credentials } from "grpc"
+import { RpcClient } from '../proto/mangostine_grpc_pb'
+import { credentials } from 'grpc'
 
-const port = 26657;
+const port = 26657
 
 export const client = new RpcClient(
     `localhost:${port}`,
--- index.ts
+++ index.ts
@@ -1,3 +1,29 @@
-import sendTx from "./Client/send-transaction"
+import sendTx from './Client/send-transaction'
+import { KeyPairKeyObjectResult, generateKeyPairSync } from 'crypto'
 
-sendTx();
+//export function generateKey() {
+export function generateKey(): KeyPairKeyObjectResult {
+    let keyOptions = {
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'der'
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem',
+        }
+    }
+    return generateKeyPairSync('ed25519', keyOptions)
+}
+
+
+export function toArrayBuffer(buf) {
+    var ab = new ArrayBuffer(buf.length)
+    var view = new Uint8Array(ab)
+    for (var i = 0; i < buf.length; ++i) {
+        view[i] = buf[i]
+    }
+    return view
+}
+
+sendTx()
